d594f76e05d95845274a9d61607d66d4162a7787
esoroush <e.soroush@hotmail.com>
2016-10-06 18:52:20 +0330

    handles one video file

--- calculateVideoDurations.sh
+++ calculateVideoDurations.sh
@@ -1,20 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 # this script calculates total duration of videos and maybe audios on the current directory or given directory
 if [ -z "$1" ]
 then
-	DIRECTORY=*
+	DIRECTORY=.
 else
-	DIRECTORY=$1/*
+	if [ -d $1 ]; then
+		DIRECTORY=$1
+	elif [ -f $1 ]; then
+		FILE=$1
+	else
+		echo "file or directory not exists!"
+		exit 1
+	fi
 fi
 round()
 {
 	echo $(printf %.$2f $(echo "scale=$2;(((10^$2)*$1)+0.5)/(10^$2)" | bc))
 };
+if [ -z $FILE ]; then
+	FILES=`ls $DIRECTORY`
+elif [ -z $DIRECTORY ]; then
+	FILES=$FILE
+fi
 totalTime=0
-for file in $DIRECTORY
+for file in $FILES
 do
-	echo $file ...
-	totalTime=$totalTime+$(ffprobe -select_streams v -show_streams $file 2>/dev/null | grep duration= | cut -d"=" -f2)
+	videoTime=$(ffprobe -select_streams v -show_streams `echo $PWD/$file` 2>/dev/null | grep duration= | cut -d"=" -f2)
+	echo $file lasts $videoTime seconds
+	totalTime=$totalTime+$videoTime
 done
 totalTime=${totalTime}0
 totalSeconds=`echo $totalTime | bc`
